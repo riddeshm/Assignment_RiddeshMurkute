c2e39e038c89ce3392485e946445d2108a72b7a8
riddeshm <35489487+riddeshm@users.noreply.github.com>
2023-05-16 10:46:09 +0530

    Added cell and board scripts

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Board
+{
+    private int rows, cols;
+    private Cell[,] cells;
+
+    public Board(int _rows, int _cols)
+    {
+        rows = _rows;
+        cols = _cols;
+
+        cells = new Cell[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                cells[i,j] = new Cell(i, j, new Vector3(i, 0, j));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public enum CellStatus
+{
+    None,
+    Snake,
+    Particle
+}
+
+public class Cell
+{
+    private int row, col;
+    private Vector3 pos;
+    private CellStatus status;
+
+    public Cell(int _row, int _col, Vector3 _pos)
+    {
+        row = _row;
+        col = _col;
+        pos = _pos;
+    }
+
+    public int Row { get { return row; } }
+    public int Col { get { return col; } }
+    public Vector3 Pos { get { return pos; } }
+    public CellStatus Status { 
+        get { return status; }
+        set { status = value; }
+    }
+}
